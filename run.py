3b7ca0b3eccce6026e883d90de7829e6ed050bad
Marcin Marciszewski <marciszewski.m@gmail.com>
2019-05-05 12:01:50 +0000

    Edit cuisine added

--- run.py
+++ run.py
@@ -20,9 +20,11 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html", cuisines=mongo.db.cuisines.find())
 
-@app.route('/get_recipe')
-def get_recipe():
+
+@app.route('/get_recipes')
+def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
     
 @app.route('/add_recipe')
 def add_recipe():
@@ -33,19 +35,63 @@ def add_recipe():
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipe'))
+    return redirect(url_for('get_recipes'))
+
 
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
-    the_recipe = mongo.db.recipes.find_one({"_id:":ObjectId(recipe_id)})
+    the_recipe = mongo.db.recipes.find_one({"_id":ObjectId(recipe_id)})
     all_cuisines = mongo.db.cuisines.find()
-    return render_template('editrecipe.html', recipe = the_recipe, cuisines = all_cuisines)
+    return render_template('editrecipe.html', recipe=the_recipe, cuisines=all_cuisines)
+    
+    
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name': request.form.get('recipe_name'),
+        'cuisine_name': request.form.get('cuisine_name'),
+        'preparation_time': request.form.get('preparation_time'),
+        'cooking_time': request.form.get('cooking_time'),
+        'serves': request.form.get('serves'),
+        'ingredients': request.form.get('ingredients')
+    })
+    return redirect(url_for('get_recipes'))
+    
+    
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+    
+    
+@app.route('/get_cuisines')
+def get_cuisines():
+    return render_template('cuisines.html',
+    cuisines=mongo.db.cuisines.find())
+    
+
+@app.route('/edit_cuisine/<cuisine_id>')
+def edit_cuisine(cuisine_id):
+    return render_template('editcuisine.html',
+    cuisine=mongo.db.cuisines.find_one({'_id': ObjectId(cuisine_id)}))
+    
+
+@app.route('/update_cuisine/<cuisine_id>', methods=['POST'])
+def update_cuisine(cuisine_id):
+    mongo.db.cuisines.update(
+        {'_id': ObjectId(cuisine_id)},
+        {'cuisine_name': request.form.get('cuisine_name')})
+    return redirect(url_for('get_cuisines'))
 
 @app.route("/mailto", methods=["GET","POST"])
 def mailto():
     if request.method == "POST":
         flash("Thank you {}, we have recived your message!".format(request.form["name"]))
     return render_template("mailto.html", page_title="Send a message") 
+
+
 
 
 if __name__ == "__main__":
